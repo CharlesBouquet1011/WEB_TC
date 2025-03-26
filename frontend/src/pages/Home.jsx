7ea46fa0708b752baca97b5e05c63e251f2b7f62
Anais5s <anaisboisson@gmail.com>
2025-03-26 11:11:46 +0100

    menu home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import logo from '../logo.svg';
 import Switch from '../utile/bouton_login.jsx';
-import Fond from '../utile/style.jsx';
+import Fond from '../utile/style_home.jsx';
 
 
 export default function Home(){
     return(
         <Fond>
         <div className="App">
-        <header className="App-header">
-          <p> 
-            Driving Enhanced    
-          </p>
-        </header>
+        <div className="video-container">
+        <video autoPlay loop muted playsInline className="background-video">
+         <source src={"/images/video.mp4"} type="video/mp4" />
+            Votre navigateur ne supporte pas les vidéos HTML5.
+        </video>
+        <div className="overlay">
+            <h1>Driving Enhanced</h1>
+        </div>
+        </div>
+
 
         
         <Switch classe={"profil"} page={"/login"} texte={"connexion"} />
