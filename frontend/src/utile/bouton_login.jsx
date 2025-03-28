5a8e349717ae45cfa6d7854f388ac9842479f408
Anais5s <anaisboisson@gmail.com>
2025-03-28 20:12:24 +0100

    tailwind

--- frontend/src/utile/bouton_login.jsx
+++ frontend/src/utile/bouton_login.jsx
@@ -1,32 +1,52 @@
 import { useNavigate } from "react-router";
 import { useVar } from "../Contexts/VariablesGlobales";
+
 function Switch({classe,page,texte}) {
   const {ProtocoleEtDomaine}=useVar()
   const navigate=useNavigate()
   switch (classe) {
     case "bouton":
       return (
-        <button className="btn btn-primary ombre" onClick={() => change(page, navigate)}>
+        <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => change(page, navigate)}
+        >
           {texte}
         </button>
       );
 
     case "profil":
       return (
-        <button className="btn btn-image ombre" onClick={() => change(page, navigate)}>
-          <img src={ProtocoleEtDomaine+"media/image/profil.jpg"} alt="Profil" className="button-image" />
+        <button 
+          className="relative transition-transform"
+          onClick={() => change(page, navigate)}
+        >
+          <img 
+            src={ProtocoleEtDomaine + "media/image/profil.png"} 
+            alt="Profil" 
+            className="w-10 h-10 fixed top-2 right-5"
+          />
         </button>
       );
-    
+
     case "home":
-    return (
-        <button className="btn btn-home ombre" onClick={() => change(page, navigate)}>
-        <img src={ProtocoleEtDomaine+"media/image/home.jpg"} alt="Accueil" className="button-home" />
+      return (
+        <button 
+          className="relative transition-transform"
+          onClick={() => change(page, navigate)}
+        >
+          <img 
+            src={ProtocoleEtDomaine + "media/image/home.png"} 
+            alt="Accueil" 
+            className="w-10 h-10 fixed top-2 left-5"
+          />
         </button>
-    ); 
+      );
+
     default:
-      return <p className="text-muted">Classe inconnue</p>;
+      return <p className="text-gray-500">Classe inconnue</p>;
   }
+
 }
 
 
