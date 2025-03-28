7cdd7db3c7ef4f3a85e75db371717c9442537506
Mathis <ducher.mathis@gmail.com>
2025-03-28 09:04:26 +0100

    ajout de voiture aléatoire hardcodée

--- frontend/src/Admin/components/Vehicule.js
+++ frontend/src/Admin/components/Vehicule.js
@@ -2,22 +2,6 @@ import { useState, useEffect } from 'react';
 import { Tile } from './Tile.js';
 
 // import ferrariImage from "../public/ferrari.png"; 
-// const cars = [
-//   { image: "https://www.usinenouvelle.com/mediatheque/2/3/9/001298932_1200x800_c.jpg", model: "Bugatti Chiron", plate: "FW-245-MD" },
-//   //{ image: ferrariImage, model: "Ferrari 812 Superfast", plate: "AB-763-LP" },
-//   { image: "https://www.automotivpress.fr/wp-content/uploads/2019/03/Lamborghini-Aventador-SVJ-Roadster-8.jpg", model: "Lamborghini Aventador SVJ", plate: "QR-982-ZX" },
-//   { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_IrIRnm_G2Fj-KNpDNNe70-9JPlivwQbHSA&s", model: "Rolls-Royce Phantom", plate: "XT-527-KH" },
-//   { image: "https://i.gaw.to/content/photos/41/69/416992-la-nouvelle-porsche-911-turbo-s-grimpe-a-641-chevaux.jpg", model: "Porsche 911 Turbo S", plate: "YL-318-WG" },
-//   { image: "https://i1.wp.com/pdlv.fr/wp-content/uploads/2021/11/70292-autoart-aston-martin-dbs-2.jpg?fit=1200%2C723", model: "Aston Martin DBS Superleggera", plate: "MK-654-VT" },
-//   { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScOsqXe7iLU7dHDVqHPEPb7PAfX-dad0QJFA&s", model: "Bentley Continental GT", plate: "DN-432-XC" },
-//   { image: "https://cdn.motor1.com/images/mgl/pEblJ/s3/maserati-mc20.jpg", model: "Maserati MC20", plate: "ZH-871-BQ" },
-//   { image: "https://mclaren.scene7.com/is/image/mclaren/765lt-3-1200x1200-1:crop-4x3?wid=1920&hei=1440", model: "McLaren 765LT", plate: "GT-609-FY" },
-//   { image: "https://carfans.fr/wp-content/uploads/2023/09/Mercedes-AMG-GT-black-series-occasion_1-scaled.jpg.webp", model: "Mercedes-AMG GT Black Series", plate: "PJ-728-RM" },
-//   { image: "https://lesvoitures.fr/wp-content/uploads/2018/07/audi-r8-v10-plus-audi-sport-performance-2018.jpg", model: "Audi R8 V10 Performance", plate: "LX-875-DP" },
-//   { image: "https://auto.cdn-rivamedia.com/photos/annoncecli/big/bmw-m8-competition-coupe-157953642.jpg", model: "BMW M8 Competition", plate: "KY-562-TB" },
-//   { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLR_fpXtvlGl8FKdzpbIPeXuue0DIUZsoGHA&s", model: "Jaguar F-Type R", plate: "OS-903-LN" },
-//   { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqKsjrD7AVwpiCotPiIxBEOtAa-jbDIeChEw&s", model: "Lexus LC 500", plate: "EV-146-QZ" }
-// ];
 
 const carList = [
   {
@@ -25,7 +9,7 @@ const carList = [
     "modele": "Chiron",
     "plaque": "FW-245-MD",
     "imageURL": "https://www.usinenouvelle.com/mediatheque/2/3/9/001298932_1200x800_c.jpg",
-    "prix": 485,
+    "prix": 489,
     "carburant": "Diesel",
     "transmission": "Manuelle",
     "description": "Excellente"
@@ -35,7 +19,7 @@ const carList = [
     "modele": "Aventador SVJ",
     "plaque": "QR-982-ZX",
     "imageURL": "https://www.automotivpress.fr/wp-content/uploads/2019/03/Lamborghini-Aventador-SVJ-Roadster-8.jpg",
-    "prix": 956,
+    "prix": 959,
     "carburant": "Essence",
     "transmission": "Automatique",
     "description": "Ouverture papillon"
@@ -69,6 +53,86 @@ const carList = [
     "carburant": "Essence",
     "transmission": "Manuelle",
     "description": "Élégante"
+  },
+  {
+    "marque": "Bentley",
+    "modele": "Continental GT",
+    "plaque": "DN-432-XC",
+    "imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScOsqXe7iLU7dHDVqHPEPb7PAfX-dad0QJFA&s",
+    "prix": 789,
+    "carburant": "Essence",
+    "transmission": "Manuelle",
+    "description": "Standing"
+  },
+  {
+    "marque": "Maserati",
+    "modele": "MC20",
+    "plaque": "ZH-871-BQ",
+    "imageURL": "https://cdn.motor1.com/images/mgl/pEblJ/s3/maserati-mc20.jpg",
+    "prix": 359,
+    "carburant": "Électrique",
+    "transmission": "Automatique",
+    "description": "Bonne accélération"
+  },
+  {
+    "marque": "McLaren",
+    "modele": "765LT",
+    "plaque": "GT-609-FY",
+    "imageURL": "https://mclaren.scene7.com/is/image/mclaren/765lt-3-1200x1200-1:crop-4x3?wid=1920&hei=1440",
+    "prix": 569,
+    "carburant": "Électrique",
+    "transmission": "Automatique",
+    "description": "Dynamique"
+  },
+  {
+    "marque": "Mercedes-AMG",
+    "modele": " GT Black Series",
+    "plaque": "PJ-728-RM",
+    "imageURL": "https://carfans.fr/wp-content/uploads/2023/09/Mercedes-AMG-GT-black-series-occasion_1-scaled.jpg.webp",
+    "prix": 189,
+    "carburant": "Essence",
+    "transmission": "Automatique",
+    "description": "Coffre électrique"
+  },
+  {
+    "marque": "Audi",
+    "modele": " R8 V10 Performance",
+    "plaque": "LX-875-DP",
+    "imageURL": "https://lesvoitures.fr/wp-content/uploads/2018/07/audi-r8-v10-plus-audi-sport-performance-2018.jpg",
+    "prix": 349,
+    "carburant": "Essence",
+    "transmission": "Automatique",
+    "description": "Pratique"
+  },
+  {
+    "marque": "BMW",
+    "modele": "M8 Competition",
+    "plaque": "KY-562-TB",
+    "imageURL": "https://auto.cdn-rivamedia.com/photos/annoncecli/big/bmw-m8-competition-coupe-157953642.jpg",
+    "prix": 379,
+    "carburant": "Essence",
+    "transmission": "Manuelle",
+    "description": "Sport"
+  },
+  {
+    "marque": "Jaguar",
+    "modele": "F-Type R",
+    "plaque": "OS-903-LN",
+    "imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLR_fpXtvlGl8FKdzpbIPeXuue0DIUZsoGHA&s",
+    "prix": 329,
+    "carburant": "Essence",
+    "transmission": "Manuelle",
+    "description": "Classe"
+  },
+  {
+    "marque": "Lexus",
+    "modele": "LC 500",
+    "plaque": "EV-146-QZ",
+    "imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqKsjrD7AVwpiCotPiIxBEOtAa-jbDIeChEw&s",
+    "prix": 299,
+    "carburant": "Électrique",
+    "transmission": "Automatique",
+    "description": "Légère"
   },
 ];
 
