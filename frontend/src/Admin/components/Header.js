1cb2c2b5392fa5642548ce5e5bd79dd53d97d531
Mathis <ducher.mathis@gmail.com>
2025-04-01 01:38:49 +0200

    patch bouton admin + modification des voitures

--- frontend/src/Admin/components/Header.js
+++ frontend/src/Admin/components/Header.js
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function Header({ activeTab, setActiveTab }) {
   const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setOpenMenu(false);
+      } else {
+        setOpenMenu(true);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <nav style={{ backgroundColor: "#7fb3d5" }} className="navbar navbar-expand-lg nav-underline">
       <div className="container-fluid">
         <a className="navbar-brand">Driving Enhanced</a>
-        <button
-            className="navbar-toggler"
-            type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setOpenMenu(!openMenu)}
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" onClick={() => setOpenMenu(!openMenu)}>
+        <span className="navbar-toggler-icon"></span>
         </button>
-        <div className={`collapse navbar-collapse ${openMenu ? "show" : ""}`} id="navbarSupportedContent">
+        <div className={`navbar-collapse ${openMenu ? "collapse" : ""}`} id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <button className={`nav-link ${activeTab===0 ? "fw-bold" : ""}`} onClick={() => setActiveTab(0)}>Véhicules</button>
