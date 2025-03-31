a1e9bc9ba590021d3ebd4b7f3585d72501b931a9
cb <charlesbouquet1011@gmail.com>
2025-03-31 22:07:08 +0200

    affichage responsive (testé sur tous les appareils proposés par défaut)

--- frontend/src/Cars/Cars.jsx
+++ frontend/src/Cars/Cars.jsx
@@ -37,7 +37,7 @@ export default function Cars(){
         
         return(
             <Fond>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-8 bg-gray-50">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-8 bg-gray-50">
                 
                 {Voitures.map((care, index)=>(
                     
