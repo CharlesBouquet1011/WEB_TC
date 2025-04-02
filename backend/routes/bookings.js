5266e4e068aaea51e9fcf15866e90f0be8582eeb
cb <charlesbouquet1011@gmail.com>
2025-04-02 20:06:57 +0200

    ajout admin + /modify/delete booking

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const csrfProtection=require("../config/csrf.js");
+var Math = require('mathjs');
 
 const User = require("../models/UserModel.js");
 const limiter = require("../config/rateLimiter.js");
@@ -53,21 +54,6 @@ router.get("/infos",csrfProtection,async(req,res)=>{
 })
 
 
-router.delete("/delete",csrfProtection,limiter,async (req,res) => {
-    try{
-        const {idBooking} = req.body
-        if (typeof idBooking !=="string"){
-            res.status(400).json({erreur: "Id Invalide"})
-        }
-        const deletedBooking=await Booking.findByIdAndDelete({ _id: { $eq: idBooking } }) //suppression de l'utilisateur
-        console.log("Réservation supprimée")
-        res.status(200).json({message: "Réservation supprimée"})
-    }catch (err){
-        res.status(500)
-        console.log("Erreur: ", err)
-    }
-})
-
 router.get("/see",csrfProtection,auth,limiter, async (req,res)=>{
     try {
         const {userId} = req.user //on peut prendre userId parce qu'on l'a mis dans le login avec jwt
@@ -79,6 +65,127 @@ router.get("/see",csrfProtection,auth,limiter, async (req,res)=>{
         console.log("Erreur: ",err)
     }
 
+})
+
+router.post("/modify",csrfProtection,auth,limiter,async(req,res)=>{
+    const {userId}=req.user
+    var {idBooking,nvDateDebut,nvDateFin,idnvVoitureReservee}=req.body
+    
+    
+    
+    
+    if (typeof idBooking !=="string"){
+        res.status(400).json({erreur: "Id Invalide"})
+        return ;
+    }
+    const concernedBooking=await Booking.findById({_id:{$eq:idBooking}})
+    if (!concernedBooking.validated){
+        res.status(200).json({message:"Veuillez d'abord payer votre Réservation ou l'annuler"})
+        return ;
+    }
+    
+    if (concernedBooking.user!=userId){
+        res.status(403).json({erreur:"Permission incorrect"})
+        return ;
+    }
+
+    //gestion de s'il laisse les champs vides
+    if (!nvDateDebut){
+        nvDateDebut=concernedBooking.dateDebut
+    }
+    if (!nvDateFin){
+        nvDateFin=concernedBooking.dateFin
+    }
+    if (!idnvVoitureReservee){
+        idnvVoitureReservee=concernedBooking.voitureReservee
+    }
+
+
+    const ancienneDuree=Math.ceil((Booking.dateDebut-Booking.dateFin)/(1000*60*60*24))
+    const nvlleDuree=Math.ceil((nvDateFin-nvDateDebut)/(1000*24*60*60))
+    
+    if ( nvlleDuree!= ancienneDuree ||idnvVoitureReservee!=concernedBooking.voitureReservee){
+        var ancienPrix=0//il faudrait recalculer l'ancien prix ou l'avoir stocké
+        rembourse(ancienPrix)
+        var prix=0 //il faudrait calculer le prix en fonction de la voiture vraiseblablement
+        faitPayer(prix)
+    }
+    const session = await mongoose.startSession(); // Démarre une session
+    session.startTransaction();
+    try{
+        concernedBooking.dateDebut=nvDateDebut
+        concernedBooking.dateFin=nvDateFin
+        concernedBooking.voitureReservee=idnvVoitureReservee
+        await concernedBooking.save({ session }); 
+
+    await session.commitTransaction(); 
+    session.endSession();
+    res.status(200).json({message:"Modification réussie"})
+    } catch (err){
+        console.log("Erreur lors du changement de la réservation :", err)
+        await session.abortTransaction(); // Annule la transaction en cas d'erreur
+        session.endSession();
+        res.status(500).json({erreur:"Erreur serveur"})
+    }
+    
+    
+
+})
+//il faudrait rembourser mais pour l'instant on n'a pas de façon de payer
+function rembourse(){
+
+}
+function calculeRemboursement(){
+
+}
+//pour le coup il faudrait remplir cette fonction là, on peut le faire
+function calculePaiement(){
+
+}
+//il faudrait faire payer le prix indiqué
+function faitPayer(prix){
+
+}
+
+
+router.delete("/delete",csrfProtection,auth,limiter, async (req,res)=>{
+    try{
+        const {userId}=req.body
+        const user=User.findById(userId)
+        const {idBooking}=req.body
+        
+        if (!user.admin){//si ce n'est pas un utilisateur admin
+            if (typeof idBooking !=="string"){
+                res.status(400).json({erreur: "Id Invalide"})
+            }else{
+                const booking=Booking.findById({_id:{$eq:idBooking}})
+                //calculer le prix et rembourser en plus, on n'a pas besoin de s'en occuper ici n'ayant pas de plateforme de paiement
+
+
+                if (booking.user!=userId){
+                    res.status(403).json({erreur:"Vous n'êtes pas le propriétaire de la réservation"})
+                }
+                else{
+                    const deletedBooking=await Booking.findByIdAndDelete({ _id: { $eq: idBooking } }) //suppression de l'utilisateur
+                    res.status(200).json({message:"Réservation supprimée"})
+                }
+            }
+
+        }else {
+            if (typeof idBooking !=="string"){
+                res.status(400).json({erreur: "Id Invalide"})
+            }
+             //calculer le prix et rembourser aussi   
+                const deletedBooking=await Booking.findByIdAndDelete({ _id: { $eq: idBooking } }) //suppression de l'utilisateur
+                console.log("Réservation supprimée")
+                res.status(200).json({message: "Réservation supprimée"})
+            
+        }
+    }catch (err){
+            res.status(500)
+            console.log("Erreur: ", err)
+        }
+    
 })
 
 module.exports=router
